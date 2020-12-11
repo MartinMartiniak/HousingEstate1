0fcb6990e9626817d3d0a7304d903dcacca25ea7
Martin Martiniak <martiniak.martin15@gmail.com>
2020-12-11 20:48:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HousingEstate1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
