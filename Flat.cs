5e55929539260ddfbe7e383aeb1df07d90435e52
Martin Martiniak <martiniak.martin15@gmail.com>
2020-12-11 21:37:48 +0100

    Make a class "Flat" which consists of Name, FlatNumber, it's Extent and number of rooms. Make a flat and check if it works.

--- Flat.cs
+++ Flat.cs
@@ -6,5 +6,30 @@ namespace HousingEstate1
 {
     class Flat
     {
+        public string Name { get; set; }
+        public int FlatNum { get; set; }
+        public int Extent { get; set; }
+        public int Rooms { get; set; }
+
+
+        public Flat(string name, int flatNum, int extent, int rooms)
+        {
+            Name = name;
+            FlatNum = flatNum;
+            Extent = extent;
+            Rooms = rooms;
+
+        }
+        public Flat()
+        {
+            Name = "-1";
+            FlatNum = -1;
+            Extent = -1;
+            Rooms = -1;
+        }
+        public override string ToString()
+        {
+            return $"{Name}\n{FlatNum} {Extent} {Rooms}";
+        }
     }
 }
